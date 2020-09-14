aa348c0772620fab28af52d6c70add7ce8d63400
unknown <mgniadek@interia.pl>
2020-09-14 21:03:13 +0200

    Hoisting up from a ledge

--- Assets/ThirdPersonController/Player States/LedgeClimbingState.cs
+++ Assets/ThirdPersonController/Player States/LedgeClimbingState.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace ThirdPersonController
 {
@@ -14,8 +15,39 @@ namespace ThirdPersonController
         ThirdPersonMovement.LedgeInfo ledgeInfo;
         Vector3 wallDirection = new Vector3();
 
+        IEnumerator moveCoroutine = null;
+        bool endingClimb = false;
+
         public override PlayerState Process(Vector3 velocityRelativeToCamera)
         {
+            if (endingClimb)
+            {
+                moveCoroutine.MoveNext();
+                movement.rigidbody.velocity = new Vector3();
+                var state = movement.animator.GetCurrentAnimatorStateInfo(0);
+                if (!state.IsName("Ledge Climb") && !state.IsName("End Ledge Climb"))
+                {
+                    movement.animator.applyRootMotion = false;
+                    moveCoroutine = null;
+                    movement.collider.gameObject.SetActive(true);
+                    return movement.walkingState;
+                }
+                return this;
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftShift) &&
+                movement.TimeSinceStateChange > 0.1f)
+            {
+                movement.animator.applyRootMotion = true;
+                movement.animator.CrossFade("End Ledge Climb", 0.2f);
+                moveCoroutine = MoveDuringEndClimb(yVelocity1: 1.7f, time1: 0.18f,
+                                                    time2: 0.34f);
+                endingClimb = true;
+                movement.collider.gameObject.SetActive(false);
+                movement.rigidbody.velocity = new Vector3();
+                return this;
+            }
+
             if (Input.GetKeyDown(KeyCode.LeftControl))
             {
                 movement.animator.CrossFade("Fall", 0.1f);
@@ -59,10 +91,23 @@ namespace ThirdPersonController
             return this;
         }
 
-        public override void FixedProcess(Vector3 velocityRelativeToCamera) { }
+        public override void FixedProcess(Vector3 velocityRelativeToCamera)
+        {
+            if (endingClimb)
+            {
+                // There is some kind of force in positive Z. No idea what.
+                // That's why there's a counter movement
+                var state = movement.animator.GetCurrentAnimatorStateInfo(0);
+                if (state.IsName("End Ledge Climb"))
+                {
+                    movement.transform.Translate(0f, 0f, -0.03f);
+                }
+            }
+        }
 
         protected override void EnterImpl()
         {
+            endingClimb = false;
             movement.rigidbody.velocity = new Vector3();
             movement.CheckLedge(out ledgeInfo);
 
@@ -79,19 +124,25 @@ namespace ThirdPersonController
 
         void HandleRotationAndPosition(bool immediate)
         {
+            Vector3 bracedGrabOffset3 = bracedGrabOffset.y * Vector3.up +
+                                        bracedGrabOffset.x * movement.model.forward;
+
+            Vector3 hangGrabOffset3 = hangGrabOffset.y * Vector3.up +
+                                      hangGrabOffset.x * movement.model.forward;
+
             // Set position
             movement.transform.position =
-                    ledgeInfo.horizontalInfo.point - GrabOffset(bracedGrabOffset);
+                    ledgeInfo.horizontalInfo.point - bracedGrabOffset3;
 
             // Set local position depending on free hang
             Vector3 targetLocalPos =
-                ledgeInfo.freeHang ? -GrabOffset(hangGrabOffset) : new Vector3();
+                ledgeInfo.freeHang ? -hangGrabOffset3 : new Vector3();
 
             if (immediate) movement.model.localPosition = targetLocalPos;
             else movement.model.localPosition = Vector3.MoveTowards(
                                 movement.model.localPosition,
                                 targetLocalPos,
-                                GrabOffset(hangGrabOffset).magnitude * Time.deltaTime
+                                hangGrabOffset3.magnitude * Time.deltaTime
                             );
 
             // Set rotation
@@ -110,8 +161,28 @@ namespace ThirdPersonController
                 Vector3.Cross(ledgeInfo.horizontalInfo.normal, Vector3.up).normalized;
         }
 
-        Vector3 GrabOffset(Vector2 vec) =>
-            vec.y * Vector3.up +
-            vec.x * movement.model.forward;
+        IEnumerator MoveDuringEndClimb(float yVelocity1, float time1, float time2)
+        {
+            Vector3 velocity = new Vector3(0f, yVelocity1, 0f);
+            float currentTime = 0f;
+            bool secondStage = false;
+            while (true)
+            {
+                if (currentTime >= time1 + time2) break;
+                else if (!secondStage && currentTime >= time1)
+                {
+                    secondStage = true;
+                    velocity = -ledgeInfo.horizontalInfo.normal * 3f;
+
+                    float yTarget = ledgeInfo.verticalInfo.point.y - 1.7f;
+                    float remainingDistance = yTarget - movement.transform.position.y;
+                    velocity.y = remainingDistance / time2;
+                }
+
+                currentTime += Time.deltaTime;
+                movement.transform.Translate(velocity * Time.deltaTime);
+                yield return null;
+            }
+        }
     }
 }
